65b5307e8bc2d002bafe53e3cd3951139f4fa5c8
Ghulam Murtaza <ghulammurtazagopang3@gmail.com>
2023-10-08 11:27:25 +0500

    done with login screen

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -4,6 +4,7 @@ import 'package:lets_chat/themes/colors.dart';
 import 'package:lets_chat/widgets/custom_float_button.dart';
 import 'package:lets_chat/widgets/custom_text_field.dart';
 import 'package:rounded_loading_button/rounded_loading_button.dart';
+import '../widgets/custom_dialog.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -44,106 +45,143 @@ class _LoginPageState extends State<LoginPage> {
       floatingActionButtonLocation:
           FloatingActionButtonLocation.miniCenterFloat,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              height: MediaQuery.of(context).size.height,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    SizedBox(
-                      height: 100,
-                      child: Center(
-                          child: Image.asset(
-                        'assets/icons/chat.png',
-                        color: AppColor.primary,
-                      )),
-                    ),
-                    const Text(
-                      'Login',
-                      style:
-                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                    ),
-                    const Gap(50),
-                    CustomTextField(
-                      controller: _emailController,
-                      hintText: 'Email',
-                      leadingIcon: const Icon(
-                        Icons.email_outlined,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const Divider(
-                      color: Colors.grey,
-                      height: 10,
-                    ),
-                    const Gap(10),
-                    CustomTextField(
-                      obscureText: _isHidePwd,
-                      controller: _passwordController,
-                      hintText: 'Password',
-                      leadingIcon: const Icon(
-                        Icons.email_outlined,
-                        color: Colors.grey,
-                      ),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _isHidePwd = !_isHidePwd;
-                          });
-                        },
-                        icon: Icon(
-                          _isHidePwd
-                              ? Icons.visibility_off_outlined
-                              : Icons.visibility_outlined,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    const Divider(
-                      color: Colors.grey,
-                      height: 10,
-                    ),
-                    const Gap(10),
-                    Align(
-                        alignment: Alignment.centerRight,
-                        child: GestureDetector(
-                          onTap: null,
-                          child: const Text(
-                            "Forgot Password?",
-                            style: TextStyle(
-                              color: AppColor.primary,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 14,
-                            ),
-                          ),
-                        )),
-                    const Gap(20),
-                    RoundedLoadingButton(
-                      width: MediaQuery.of(context).size.width,
-                      color: AppColor.primary,
-                      controller: _loginBtnController,
-                      onPressed: () {},
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    const Gap(20),
-                  ],
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: Center(
+                      child: Image.asset(
+                    'assets/icons/chat.png',
+                    color: AppColor.primary,
+                  )),
                 ),
-              ),
+                const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const Gap(50),
+                CustomTextField(
+                  controller: _emailController,
+                  hintText: 'Email',
+                  leadingIcon: const Icon(
+                    Icons.email_outlined,
+                    color: Colors.grey,
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                  height: 10,
+                ),
+                const Gap(10),
+                CustomTextField(
+                  obscureText: _isHidePwd,
+                  controller: _passwordController,
+                  hintText: 'Password',
+                  leadingIcon: const Icon(
+                    Icons.lock_clock_outlined,
+                    color: Colors.grey,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isHidePwd = !_isHidePwd;
+                      });
+                    },
+                    icon: Icon(
+                      _isHidePwd
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility_outlined,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                  height: 10,
+                ),
+                const Gap(10),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: null,
+                      child: const Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: AppColor.primary,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                    )),
+                const Gap(20),
+                RoundedLoadingButton(
+                  width: MediaQuery.of(context).size.width,
+                  color: AppColor.primary,
+                  controller: _loginBtnController,
+                  onPressed: onLogin,
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  bool _validateForm(String email, String pwd) {
+    if (email.isEmpty || pwd.isEmpty) {
+      _loginBtnController.reset();
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const CustomDialogBox(
+            descriptions: "Please enter your email and password.",
+          );
+        },
+      );
+      return false;
+    }
+    return true;
+  }
+
+  Future onLogin() async {
+    FocusScope.of(context).unfocus();
+    if (!_validateForm(_emailController.text, _passwordController.text)) {
+      return;
+    }
+
+    // var res = await service.signInWithEmailPassword(
+    //   _emailController.text,
+    //   _passwordController.text,
+    // );
+    // if (res.status) {
+    //   _loginBtnController.success();
+    // }
+    else {
+      _loginBtnController.reset();
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const CustomDialogBox(
+            title: "Login",
+            //  descriptions: res.message,
+          );
+        },
+      );
+    }
   }
 }
